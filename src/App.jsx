169ba1dbe70151c61621e5366892a3882249bfca
cyrshadow <150596216+Cyroshadow@users.noreply.github.com>
2026-04-19 16:02:14 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { Heart, Search, Home } from 'lucide-react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Heart />
+      <Home />
+      <Search />
+    </>
+  )
+}
+
+export default App
